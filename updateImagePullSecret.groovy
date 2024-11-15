5c9f8e2d2b28a399c4ba45025eb1b28561983b0e
sergey.alekseev <demiurg58@gmail.com>
2024-11-15 15:35:13 +0300

    add script compare token

--- updateImagePullSecret.groovy
+++ updateImagePullSecret.groovy
@@ -21,6 +21,22 @@ pipeline {
         stage('Get current OSE Token') {
             steps {
                 script {
+                    def secretData = sh(
+                        script: "oc get secret test-secret -o json | jq -r '.data[\".dockerconfigjson\"]'", returnStdout: true
+                    ).trim()
+
+                    def decodeToken = new String(secretData.decodeBase64())
+                    sh "echo '${decodeToken}' > file.json"
+
+                    def jsonData = readJSON file: "file.json"
+                    def password = jsonData.auth['registry.nexus.ru'].password
+                    env.CURRENT_PULL_PASSWORD = password
+                }
+            }
+        }
+        stage('Get credentials') {
+            steps {
+                script {
 
                 }
             }
