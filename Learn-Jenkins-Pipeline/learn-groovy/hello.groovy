0d5c4ec84b09f93c87d63a03adaf71a21ba5c107
sergey.alekseev <demiurg58@gmail.com>
2025-01-07 20:33:28 +0300

    add groovy lessons

---
+++ Learn-Jenkins-Pipeline/learn-groovy/hello.groovy
@@ -0,0 +1,12 @@
+def hello = "Hello"
+def name = "John"
+println(hello + " " + name) // Это комментарий
+
+/*
+Это
+многострочный
+комментарий
+ */
+/** А
+ * Это уже
+ * groovydoc*/
---
+++ Learn-Jenkins-Pipeline/learn-groovy/vars.groovy
@@ -0,0 +1,16 @@
+def x = 10
+def y = 'Hello World'
+def z = 0.5
+println(x * z)
+println(y)
+def b = true
+println(b)
+
+def map = [:]
+map."name" = "John"
+map."age" = 25
+
+println(map)
+
+println(y.getClass())
+
