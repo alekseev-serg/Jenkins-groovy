41dba7c197a85da3c05a815956e13517c7493db4
sergey.alekseev <demiurg58@gmail.com>
2025-01-12 10:07:06 +0300

    learm groovy map, list, range

--- Learn-Jenkins-Pipeline/part-1-learn-groovy/list.groovy
+++ Learn-Jenkins-Pipeline/part-1-learn-groovy/list.groovy
@@ -10,6 +10,39 @@ exampleList[5] = 5
 exampleList << 6 << 7
 exampleList += [8, 9 , 1000000]
 
+
+def numbers = [0, 1, "Hello", 3, true]  
+println numbers[0]  // обращение к элементу по индексу
+
+println numbers[0..3]
+  
+def empty = []  // Создание пустого списка
+
+// добавление в индекс списка
+numbers[3] = 1000
+// добавление в конец списка
+numbers << 100
+numbers << 1001
+println(numbers)
+println numbers += [10, 20]
+println '----------------------------------------------------------'
+
+// Итерации по списку
+[100, 2000, 30000, 400000].each {
+    println "Item: $it" // it - неявный параметр, соответствующий текущему элементу
+}
+
+['Valera', 'Vovan', 'Petro', 'Dmitro'].eachWithIndex { 
+    it, i -> println "$i: $it"  // it — текущий элемент,  i — индекс
+}
+
 // Перебор списка
 for ( iter in exampleList) { println iter }
 println '----------------------------------------------------------'
+
+// Методы списка
+numbers = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+def flattenedNumbers = numbers.flatten()
+println(flattenedNumbers)
+
+// Методы: add, addAll, contains, get, getAt, intersect, isEmpty, leftShift
