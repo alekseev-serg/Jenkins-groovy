cc99b707a3b89d2f98b08aa78fd1a6b1b4182f83
sergey.alekseev <demiurg58@gmail.com>
2025-01-12 12:33:07 +0300

    learn groovy Methods

--- Learn-Jenkins-Pipeline/part-1-learn-groovy/function.groovy
+++ Learn-Jenkins-Pipeline/part-1-learn-groovy/function.groovy
@@ -9,6 +9,8 @@ def paramMethod(param1) {
     "Parameter: $param1"
 }
 
+println(paramMethod('Hello'))
+
 static String strParamMethod(String arg1) {
     println "This is string: $arg1"
 }
@@ -21,4 +23,17 @@ def paramDefault(String a, Integer b = 100, def c = 500) {
 
 println 'strMethod(): ' + strMethod()
 println 'paramMethod(): ' + paramMethod(2340)
+
 paramDefault('Hello')
+
+def add(int a, int b) {
+    return a + b
+}
+
+println(add(4,5))
+
+// Переменное число аргументов
+def check(Object[] args)  { args.length }
+println check() == 0 // true
+println check(1) == 1 // true
+println check(1, 2) == 2 // true
