fb28519d44ef6a159fd01c10c621c3fb49eb50bc
sergey.alekseev <demiurg58@gmail.com>
2025-02-25 20:38:22 +0300

    write scripted pipeline for build django backend

--- Learn-Jenkins-Pipeline/shared-lib-use-case/example-4.groovy
+++
@@ -1,4 +0,0 @@
-@Library('test-library') _
-
-logs.debug('check var')
-logs.info('10005000')
---
+++ vars/runPipeline.groovy
@@ -0,0 +1,23 @@
+def call (){
+    echo "Hello World"
+
+    node("builder") {
+        def webhookpayload = readJSON text: env.JSON_PAYLOAD;
+        echo "JSON: ${webhookpayload}"
+
+        currentBuild.displayName =  '#' + env.BUILD_NUMBER;
+
+        stage('Get code'){
+            echo "Get SCM"
+            sh "ls -la"
+        }
+
+        stage('Сборка'){
+            echo "Build distrib"
+        }
+
+        stage('Deploy'){
+            echo "Deploy"
+        }
+    }
+}
