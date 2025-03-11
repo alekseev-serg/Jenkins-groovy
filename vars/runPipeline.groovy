444f97202dede9238b5b9acf6bd810b2a7500dc5
sergey.alekseev <demiurg58@gmail.com>
2025-03-11 20:56:56 +0300

    work with json

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -2,15 +2,24 @@ def call (){
 
     node("builder") {
 
+        currentBuild.displayName =  '#' + env.BUILD_NUMBER;
         echo "Raw JSON: ${env.JSON_PAYLOAD}"
 
         echo "---------------------"
 
         def webhookpayload = readJSON text: env.JSON_PAYLOAD;
-
         echo "Read JSON: ${webhookpayload}";
 
-        currentBuild.displayName =  '#' + env.BUILD_NUMBER;
+
+                if (webhookpayload instanceof List) {
+            echo "JSON пришёл в виде массива! Количество элементов: ${webhookpayload.size()}"
+            // Например, обработаем первый элемент:
+            def firstItem = webhookpayload[0]
+            echo "Первый элемент: ${firstItem}"
+        } else {
+            echo "JSON пришёл в виде объекта!"
+            echo "Repository: ${webhookpayload.repository.full_name}"
+        }
 
         stage('Get code'){
             echo 'clone repo from webhook'
