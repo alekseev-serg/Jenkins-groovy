eaae75d852df159147ee33309b6085400eb7ae90
sergey.alekseev <demiurg58@gmail.com>
2025-03-13 20:45:37 +0300

    learn jenkins

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -3,26 +3,11 @@ def call (){
     node("builder") {
 
         currentBuild.displayName =  '#' + env.BUILD_NUMBER;
-        echo "Raw JSON: ${env.JSON_PAYLOAD}"
-
         echo "---------------------"
-
-        def webhookpayload = readJSON text: env.JSON_PAYLOAD;
+        def webhookpayload = env.JSON_PAYLOAD;
         echo "Read JSON: ${webhookpayload}";
+        echo "Repository: ${webhookpayload.repository.full_name}"
 
-        if (webhookpayload instanceof List) {
-            echo "JSON пришёл в виде массива! Количество элементов: ${webhookpayload.size()}"
-            // Например, обработаем первый элемент:
-            def firstItem = webhookpayload[0]
-            echo "Первый элемент: ${firstItem}"
-        } else {
-            echo "JSON пришёл в виде объекта!"
-            echo "Repository: ${webhookpayload.repository.full_name}"
-        }
-
-        def jsonProjects = new groovy.json.JsonBuilder(webhookpayload).toPrettyString();
-
-        echo "jsonProjects: ${webhookpayload}";
 
         stage('Get code'){
             echo 'clone repo from webhook'
@@ -40,6 +25,7 @@ def call (){
             echo "Deploy"
         }
     }
+
     post {
         always {
             script {
