e0b00413d140fa792236331f8dbc18ec2a85e82e
sergey.alekseev <demiurg58@gmail.com>
2025-04-02 10:02:11 +0300

    add generate pipeline

---
+++ vars/front_register_app.groovy
@@ -0,0 +1,105 @@
+@groovy.transform.Field def cfg = [
+    workDir: 'folder',
+    repository: [
+        sshUrl: 'ssh://git....some.url.git',
+        branch: 'main',
+        credentialsId: 'credentials'
+    ],
+    subsystemPath: './ALPHA/DEV/subsystem.json'
+];
+
+def call(){
+    validate();
+
+    dir(cfg.workDir){
+
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: cfg.repository.branch]],
+            userRemoteConfigs: [[
+                url: cfg.repository.sshUrl,
+                credentialsId: cfg.repository.credentialsId,
+            ]]
+        ])
+
+        def projects = readJSON file cfg.subsystemPath;
+
+        if(projects[env.FORMAT_APP_NAME] != null){
+            echo "APP is always exist";
+            return;
+        }
+
+        echo "APP not exist. CREATE APP";
+
+        projects.remove("__default");
+        def uri = new URI(params.GIT_SSH_URL);
+
+        projects[env.FORMAT_APP_NAME] = [
+            name: "value",
+            name: "value",
+            name: "value",
+            name: "value",
+            name: "value",
+            name: "value",
+            meta: [
+                name: "value",
+                name: "value",
+                name: "value",
+                name: "value",
+                name: "value",
+            ]
+        ]
+
+        //Записываем данные проекта в файл
+        ['SEGMENT1', 'SEGMENT2'].each{segment ->
+            ['DEV', 'STABLE', 'PREPROD', 'PROD'].each{stand ->
+                try{
+                    def rawProject = readJSON file: "${segment}/${stand}/subsystem.json";
+                    def jsonProjects = new groovy.json.JsonBuilder([__default: rawProject])
+                }catch(Exception e){
+                    echo "Error ${e.message}";
+                }
+            }
+        }
+        //Пушим в репозиторий
+    }
+}
+
+def getProject(){
+    validate();
+
+    def repoExist = fileExists cfg.workDir + "README.md";
+    def project = null;
+
+    dir(cfg.workDir){
+
+        if(!repoExists){
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: cfg.repository.branch]],
+                userRemoteConfigs: [[
+                    url: cfg.repository.sshUrl,
+                    credentialsId: cfg.repository.credentialsId,
+                ]]
+            ])
+        }
+
+        def projects = readJSON file cfg.subsystemPath;
+
+        project = projects[env.FORMAT_APP_NAME];
+    }
+
+    return project;
+}
+
+def validate(){
+    if (params.APP_NAME.size() > 17) throw new Exception("APP_NAME max size 17");
+    if (params.APP_NAME.startWith("Z_")) throw new Exception("APP_NAME must not contains Z_");
+    if (!params.GIT_SSH_URL.startWith("ssh://")) throw new Exception("GIT_SSH_URL must contains ssh://");
+    if (params.APP_DESCRIPTION.size() == 0) throw new Exception("write APP_DESCRIPTION");
+
+    env.FORMAT_APP_NAME = params.APP_NAME.toUpperCase().replace("\\","_").replace("-","_");
+
+    currentBuild.displayName = "${env.FORMAT_APP_NAME}";
+    currentBuild.description = "Try to registry new app";
+}
---
+++ vars/generate_front.groovy
@@ -0,0 +1,52 @@
+@groovy.transform.Field def project = null;
+
+def call(){
+    node('some||worker||node'){
+        if(!env.APP_NAME){
+            stage("Реконфигурация джобы"){
+                reconfigure();
+            }
+
+            return;
+        }
+
+        stage('Registry application'){
+            front_register_app();
+        }
+
+        stage('Read app data'){
+            echo "TODO"
+        }
+
+        stage('Setup webhook git'){
+            echo "TODO"
+        }
+
+        stage('Generate base project'){
+            echo "TODO"
+        }
+
+        stage('Push to develop repo'){
+            echo "TODO"
+        }
+
+        stage('Create PR'){
+            echo "TODO"
+        }
+
+        stage('Print build info'){
+            echo "TODO"
+        }
+    }
+}
+
+def reconfigure(){
+    properties([
+        parameters([
+            string(name: "APP_NAME", description: "some description"),
+            string(name: "APP_DESCRIPTION", description: "some description"),
+            string(name: "COMMAND_CONF_LINK", description: "some description"),
+            string(name: "GIT_SSH_URL", description: "some description"),
+        ])
+    ])
+}
