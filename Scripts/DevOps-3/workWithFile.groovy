5250ffd079352351acf91847e4071b69e0b6609d
sergey.alekseev <demiurg58@gmail.com>
2025-04-06 10:15:48 +0300

    add func with param

--- Scripts/DevOps-3/workWithFile.groovy
+++ Scripts/DevOps-3/workWithFile.groovy
@@ -5,7 +5,7 @@ node {
     stage("Read From file"){
         sh "pwd"
         sh "ls -la"
-        def config = readJSON(file: 'config.json')
+        def config = readJSON(file: 'Scripts/DevOps-3/config.json')
         echo "Version = ${config.version}"
     }
 
