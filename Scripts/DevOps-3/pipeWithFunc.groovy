b32201eec9defae3d4c535d545dbd04cb7f66b13
sergey.alekseev <demiurg58@gmail.com>
2025-04-06 09:42:48 +0300

    add func with param

--- Scripts/DevOps-3/pipeWithFunc.groovy
+++ Scripts/DevOps-3/pipeWithFunc.groovy
@@ -18,6 +18,12 @@ def deploy(){
     echo "This is Deploy func"
 }
 
+def notify(status){
+    if(${status} = 'SUCCESS'){echo "Status: ${status}" return}
+    if(${status} = 'FAILURE'){echo "Status: ${status}" return}
+    if(${status} = 'ABORTED'){echo "Status: ${status}" return}
+}
+
 node{
     checkout scm
     buildApp();
@@ -28,5 +34,9 @@ node{
 
     stage('Deploy'){
         deploy();
+    }
+
+    stage('Message'){
+
     }
 }
