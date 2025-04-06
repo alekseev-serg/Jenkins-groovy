2c5d9aa0588c9a5c7c70ce79995bf48dd9f147ea
sergey.alekseev <demiurg58@gmail.com>
2025-04-06 10:33:20 +0300

    add func with param

--- Scripts/DevOps-3/workWithFile.groovy
+++ Scripts/DevOps-3/workWithFile.groovy
@@ -7,13 +7,14 @@ node {
         sh "ls -la"
         def config = readJSON(file: 'Scripts/DevOps-3/config.json')
         echo "Version = ${config.version}"
+        echo "Environment = ${config.env}"
     }
 
     def stages = ['Build', 'Test', 'Deploy']
 
     stages.each { stageName ->
         stage(stageName){
-            echo "Running stage: ${}"
+            echo "Running stage: $stageName}"
         }
     }
 }
